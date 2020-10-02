bc502357ca41c1a756cb9e72684bce3e7dff3d6f
victor tung <vtfsdev@gmail.com>
2020-10-02 09:51:37 -0500

    Bash exercise 5 - loop example

--- bash/loop.sh
+++ bash/loop.sh
@@ -7,3 +7,11 @@ for COMMAND in "${COMMANDS[@]}"
 do
   echo $COMMAND
 done
+
+# List all of the configuration files in the _/etc/apache2/sites-available/_ directory
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+for FILENAME in $VHOSTS_PATH
+do
+  echo $FILENAME
+done
